2f8b4828fe18e410d52c25a95ae1669a2b4a73c5
Rahul Gopinath <rahul@gopinath.org>
2018-03-18 02:20:05 -0700

    Init

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='PyCore',
+    version='1.0',
+    description='Pure Python Core Modules',
+    author='Rahul Gopinath',
+    author_email='rahul@gopinath.org',
+    url='http://github.com/vrthra/pycore',
+    packages=['pycore']
+    )
